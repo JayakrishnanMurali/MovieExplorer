7fdc1192a19e38392d61522e4f121e0e150ef288
jayakrishnanm <jayakrishnan.m@neoito.com>
2025-05-10 13:36:09 +0530

    feat: set up initial iOS and Android project structure with Expo
    - Added necessary configuration files for both iOS and Android platforms, including Podfile, build.gradle, and gradle.properties.
    - Established .gitignore files for iOS and Android to exclude build artifacts and temporary files.
    - Updated app.json to include platform-specific icons and bundle identifiers.
    - Implemented a splash screen and app theme for a cohesive user experience.
    - Integrated React Query for data fetching and state management in the app layout.
    - Added initial assets for icons and splash screens to enhance visual branding.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,37 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect, useState } from "react";
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
 
 const queryClient = new QueryClient();
 
 export default function RootLayout() {
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Pre-load fonts, make any API calls you need to do here
+        await new Promise((resolve) => setTimeout(resolve, 2000)); // Simulated loading time
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setIsReady(true);
+        await SplashScreen.hideAsync();
+      }
+    }
+
+    prepare();
+  }, []);
+
+  if (!isReady) {
+    return null;
+  }
+
   return (
     <QueryClientProvider client={queryClient}>
       <Stack
